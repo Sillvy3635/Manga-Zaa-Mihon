b2d8b0198b2f2a3a06eee62e4c4df42075e7659a
Sillvy3635 <tivezerza@gmail.com>
2026-08-29 17:48:04 +0700

    Fix MangaSuper Cloudflare 403

--- src/th/mangasuper/src/eu/kanade/tachiyomi/extension/th/mangasuper/MangaSuper.kt
+++ src/th/mangasuper/src/eu/kanade/tachiyomi/extension/th/mangasuper/MangaSuper.kt
@@ -1,12 +1,12 @@
 package eu.kanade.tachiyomi.extension.th.mangasuper
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
 import eu.kanade.tachiyomi.multisrc.madara.MadaraBase
+import eu.kanade.tachiyomi.multisrc.madara.MadaraNoAjax
 import keiyoushi.annotation.Source
 import java.time.format.DateTimeFormatter
 
 @Source
-abstract class MangaSuper : Madara() {
+abstract class MangaSuper : MadaraNoAjax() {
 
     override val chapterMode = MadaraBase.ChapterMode.MangaAjax
 
